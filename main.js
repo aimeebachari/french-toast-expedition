c81116d9272605ed6a780d03d80911a59f61b7a3
Aimee Bachari <aimeebachari@gmail.com>
2018-07-15 20:58:34 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let bread = 1.99;
+let milk = 3.89;
+let eggs = 2.29;
+
+let total =  bread + milk + eggs;
+
+console.log(total);
+
+let severeConditions = total * 3;
+
+console.log(severeConditions);
